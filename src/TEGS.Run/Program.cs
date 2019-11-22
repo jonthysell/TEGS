7e55e0731199cd5d18cc8c253390242e183ffa77
Jon Thysell <thysell@gmail.com>
2019-11-22 11:36:44 -0800

    Removed unnecessary usings

--- src/TEGS.Run/Program.cs
+++ src/TEGS.Run/Program.cs
@@ -29,7 +29,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
-using System.Xml;
 
 namespace TEGS.Run
 {
--- src/TEGS.Test/GraphTest.cs
+++ src/TEGS.Test/GraphTest.cs
@@ -26,7 +26,6 @@
 
 using System.IO;
 using System.Text;
-using System.Xml;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
