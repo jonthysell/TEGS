93d9f6dcf99fad35d9db143d1f58623207b4cb14
Jon Thysell <thysell@gmail.com>
2019-11-23 15:58:40 -0800

    Added native Carwash example for perf comparisons

---
+++ src/NativeExamples/Carwash/Program.cs
@@ -0,0 +1,80 @@
+﻿// 
+// Program.cs
+//  
+// Author:
+//       Jon Thysell <thysell@gmail.com>
+// 
+// Copyright (c) 2019 Jon Thysell <http://jonthysell.com>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carwash
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var simArgs = ParseArgs(args);
+
+            Simulation sim = new Simulation();
+            sim.Run(simArgs);
+        }
+
+        static SimulationArgs ParseArgs(string[] args)
+        {
+            var simArgs = new SimulationArgs();
+
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                switch (args[i].ToLower())
+                {
+                    case "--seed":
+                        simArgs.Seed = int.Parse(args[++i]);
+                        break;
+                    case "--start-parameters":
+                        string[] split = args[++i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        int[] vals = new int[split.Length];
+                        for (int j = 0; j < split.Length; j++)
+                        {
+                            vals[j] = int.Parse(split[j]);
+                        }
+                        simArgs.ParameterValues = vals;
+                        break;
+                    case "--stop-time":
+                        simArgs.StopCondition.MaxTime = double.Parse(args[++i]);
+                        break;
+                    default:
+                        throw new Exception($"Did not recognize option \"{args[i]}\"");
+                }
+            }
+
+            return simArgs;
+        }
+    }
+}
---
+++ src/NativeExamples/Carwash/Simulation.cs
@@ -0,0 +1,113 @@
+﻿// 
+// CarwashSimulation.cs
+//  
+// Author:
+//       Jon Thysell <thysell@gmail.com>
+// 
+// Copyright (c) 2019 Jon Thysell <http://jonthysell.com>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+namespace Carwash
+{
+    enum EventType
+    {
+        RUN,
+        ENTER,
+        START,
+        LEAVE,
+    }
+
+    class Simulation : SimulationBase
+    {
+        protected override EventType StartingEvent => EventType.RUN;
+
+        // User Variables
+        int QUEUE = 0;
+        int SERVERS = 0;
+
+        public Simulation() { }
+
+        protected override void ProcessEvent(EventType eventType, int[] parameterValues)
+        {
+            switch (eventType)
+            {
+                case EventType.RUN:
+                    Event_RUN(parameterValues);
+                    break;
+                case EventType.ENTER:
+                    Event_ENTER();
+                    break;
+                case EventType.START:
+                    Event_START();
+                    break;
+                case EventType.LEAVE:
+                    Event_LEAVE();
+                    break;
+            }
+        }
+
+        public void Event_RUN(int[] parameterValues)
+        {
+            // Process Parameters
+            QUEUE = parameterValues[0];
+            SERVERS = parameterValues[1];
+
+            // Edges
+            ScheduleEvent(EventType.ENTER, delay: 0, priority: 5, parameterValues: null);
+        }
+
+        public void Event_ENTER()
+        {
+            // Event Body
+            QUEUE = QUEUE + 1;
+
+            // Edges
+            ScheduleEvent(EventType.ENTER, delay: t_uniformvariate(3, 8), priority: 6, parameterValues: null);
+            if (SERVERS > 0)
+            {
+                ScheduleEvent(EventType.START, delay: 0, priority: 5, parameterValues: null);
+            }
+        }
+
+        public void Event_START()
+        {
+            // Event Body
+            SERVERS = SERVERS - 1;
+            QUEUE = QUEUE - 1;
+
+            // Edges
+            ScheduleEvent(EventType.LEAVE, delay: t_uniformvariate(5, 20), priority: 6, parameterValues: null);
+        }
+
+        public void Event_LEAVE()
+        {
+            // Event Body
+            SERVERS = SERVERS + 1;
+
+            // Edges
+            if (QUEUE > 0)
+            {
+                ScheduleEvent(EventType.START, delay: 0, priority: 5, parameterValues: null);
+            }
+        }
+    }
+}
