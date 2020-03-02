8119d3cdbf4bb1beec801697832072f6f12d1dba
Jon Thysell <thysell@gmail.com>
2020-03-02 13:41:40 -0800

    Added remaining String/Random extension methods to generated code

--- src/NativeExamples/Carwash/Program.cs
+++ src/NativeExamples/Carwash/Program.cs
@@ -253,5 +253,40 @@ namespace Carwash
         {
             return alpha + (beta - alpha) * random.NextDouble();
         }
+    
+        public static double ExponentialVariate(this Random random, double lambda)
+        {
+            return -Math.Log(1.0 - random.NextDouble()) / lambda;
+        }
+    
+        public static double NormalVariate(this Random random, double mu, double sigma)
+        {
+            double z = 0.0;
+    
+            while (true)
+            {
+                double u1 = random.NextDouble();
+                double u2 = 1.0 - random.NextDouble();
+    
+                z = (4 * Math.Exp(-0.5) / Math.Sqrt(2.0)) * (u1 - 0.5) / u2;
+    
+                if ((z * z / 4.0) <= -Math.Log(u2))
+                {
+                    break;
+                }
+            }
+    
+            return mu + z * sigma;
+        }
+    
+        public static double LogNormalVariate(this Random random, double mu, double sigma)
+        {
+            return Math.Exp(random.NormalVariate(mu, sigma));
+        }
+    }
+    
+    public static class StringExtensions
+    {
+        public static int Length(this string str) => str.Length;
     }
 }
